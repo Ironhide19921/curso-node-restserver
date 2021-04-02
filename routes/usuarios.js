fa9e25206abf964baa726ff81cc5becba3344abe
Gerardo <gera_lps@hotmail.com.ar>
2021-04-02 14:10:17 -0300

    db-validators agregado

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,10 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const  Role = require('../models/role');
 
 const { validarCampos } = require('../middlewares/validar-campos');
+const { esRoleValido,
+        emailExiste } = require('../helpers/db-validators');
 
 const { usuariosGet, 
         usuariosPut,
@@ -20,14 +21,10 @@ router.put('/:id', usuariosPut);
 router.post('/', [
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('password', 'El password debe de ser más de 6 letras').isLength({ min:6 }),
-        check('correo', 'El correo no es válido').isEmail(),
+        // check('correo', 'El correo no es válido').isEmail(),
+        check('correo').custom( emailExiste ),
         // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
-        check('rol').custom( async(rol = '') => {
-                const existeRol = await Role.findOne({ rol });
-                if( !existeRol ) {
-                        throw new Error(`El rol ${ rol } no está registrado en la BD`)
-                }
-        }),
+        check('rol').custom( esRoleValido ),
         validarCampos
 ] ,usuariosPost);
 
