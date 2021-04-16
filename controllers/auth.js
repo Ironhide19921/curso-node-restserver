42fd39a855cc16679d0a2086c2314c4f09947070
Gerardo <gera_lps@hotmail.com.ar>
2021-04-15 23:53:36 -0300

    Google signin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 
@@ -53,6 +54,58 @@ const login = async(req, res = response) => {
 
 }
 
+
+const googleSignin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await  Usuario.findOne({ correo });
+        
+        if ( !usuario ) {
+            //Tengo que crearlo
+            const data = { 
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // Si el usuario está en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+         // Generar el JWT
+         const token = await generarJWT( usuario.id );
+
+    
+
+        res.json({
+           usuario,
+           token
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+            
+    }
+  
+
+}
+
+
 module.exports = {
-    login
+    login,
+    googleSignin
 }
