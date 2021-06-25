d7c7180e47c855e9e103e35cc9c6567960306b87
Gerardo <gera_lps@hotmail.com.ar>
2021-06-24 21:24:54 -0300

    Carga y mostrar imagenes

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,15 +1,31 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { cargarArchivo } = require('../controllers/uploads');
-const { validarCampos } = require('../middlewares/validar-campos');
+
+const { validarCampos, validarArchivoSubir } = require('../middlewares');
+const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads');
+const { coleccionesPermitidas } = require('../helpers');
 
 
 const router = Router();
 
 
-router.post( '/', cargarArchivo );
+router.post( '/', validarArchivoSubir,  cargarArchivo );
+
+router.put( '/:coleccion/:id', [
+    validarArchivoSubir,
+    check('id', 'El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'] ) ),
+    validarCampos
+], actualizarImagen);
+
+router.get( '/:coleccion/:id',[
+    check('id', 'El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'] ) ),
+    validarCampos
+
+],  mostrarImagen)
 
 
 
-module.exports = router
+module.exports = router;
